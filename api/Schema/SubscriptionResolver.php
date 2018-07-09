6837b675b84f53d129ec32b40c7c2a53fdfc2b68
Sergey Kambalin <greyexpert@gmail.com>
2018-07-09 10:30:53 +0300

    Remove incomplete logic

--- api/Schema/SubscriptionResolver.php
+++ api/Schema/SubscriptionResolver.php
@@ -8,15 +8,5 @@ use GraphQL\Type\Definition\ResolveInfo;
 
 class SubscriptionResolver extends CompositeResolver
 {
-    public function resolve($root, $args, ContextInterface $context, ResolveInfo $info)
-    {
-        /**
-         * @var $subscription SubscriptionInterface
-         */
-        $subscription = parent::resolve($root, $args, $context, $info);
 
-        // TODO: implement
-
-        return $subscription;
-    }
 }
