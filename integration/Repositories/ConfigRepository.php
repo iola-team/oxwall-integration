c3591524c3a3cb1da206a198c1d153cbd23c9767
Roman Romanovsky <roman.romanovsky.work@gmail.com>
2019-08-14 11:57:22 +0300

    Update configs type

--- integration/Repositories/ConfigRepository.php
+++ integration/Repositories/ConfigRepository.php
@@ -5,6 +5,7 @@ namespace Iola\Oxwall\Repositories;
 use Iola\Api\Contract\Integration\ConfigRepositoryInterface;
 use OW;
 use OW_Config;
+use IOLA_BOL_Service;
 
 class ConfigRepository implements ConfigRepositoryInterface
 {
@@ -13,16 +14,27 @@ class ConfigRepository implements ConfigRepositoryInterface
      */
     protected $owConfig;
 
+    /**
+     * @var IOLA_BOL_Service
+     */
+    protected $service;
+
     public function __construct()
     {
         $this->owConfig = OW::getConfig();
+        $this->service = IOLA_BOL_Service::getInstance();
     }
 
     public function getAll($args)
     {
+        $configs = $this->service->getConfigs();
+
         return [
             "emailConfirmIsRequired" => (boolean) $this->owConfig->getValue("base", "confirm_email"),
-            "userApproveIsRequired" => (boolean) $this->owConfig->getValue("base", "mandatory_user_approve")
+            "userApproveIsRequired" => (boolean) $this->owConfig->getValue("base", "mandatory_user_approve"),
+            "backgroundUrl" => (string) $this->service->getFileUrl("backgroundUrl"),
+            "logoUrl" => (string) $this->service->getFileUrl("logoUrl"),
+            "primaryColor" => (string) empty($configs["primaryColor"]) ? "#5259FF" : $configs["primaryColor"],
         ];
     }
 }
