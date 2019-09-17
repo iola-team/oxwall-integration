654831d12fe6bb00cb90a7fd8c947f05cd443e49
Sergey Kambalin <greyexpert@gmail.com>
2019-09-17 13:55:11 +0600

    Add widget to Index place on activate

--- activate.php
+++ activate.php
@@ -5,5 +5,5 @@
  */
 $widgetService = BOL_ComponentAdminService::getInstance();
 $widget = $widgetService->addWidget('IOLA_CMP_AppBannerWidget', false);
-$widgetPlace = $widgetService->addWidgetToPlace($widget, BOL_ComponentService::PLACE_DASHBOARD);
-$widgetService->addWidgetToPosition($widgetPlace, BOL_ComponentService::SECTION_RIGHT);
+$widgetService->addWidgetToPlace($widget, BOL_ComponentService::PLACE_DASHBOARD);
+$widgetService->addWidgetToPlace($widget, BOL_ComponentService::PLACE_INDEX);
