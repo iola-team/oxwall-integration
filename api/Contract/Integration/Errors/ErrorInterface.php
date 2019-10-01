4a38a0bd1100f6674b12f8bdd2141b824922dd6e
Roman Romanovsky <roman.romanovsky.work@gmail.com>
2019-10-01 19:55:28 +0300

    Add license (Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International)

--- api/Contract/Integration/Errors/ErrorInterface.php
+++ api/Contract/Integration/Errors/ErrorInterface.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Copyright © 2019 iola. All rights reserved. Contacts: <hello@iola.app>
+ * iola is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * You should have received a copy of the license along with this work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>
+ */
+
 namespace Iola\Api\Contract\Integration\Errors;
 
 use GraphQL\Error\ClientAware;
--- api/Schema/Relay/EdgeObject.php
+++ api/Schema/Relay/EdgeObject.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Copyright © 2019 iola. All rights reserved. Contacts: <hello@iola.app>
+ * iola is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * You should have received a copy of the license along with this work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>
+ */
 
 namespace Iola\Api\Schema\Relay;
 
